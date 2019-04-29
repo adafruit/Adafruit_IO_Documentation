d1a35804838cb2daeadd8f58046a018b6310e553
Adam Bachman <adam.bachman@gmail.com>
2019-04-29 11:46:22 -0400

    minify HTML on build

--- config.rb
+++ config.rb
@@ -44,6 +44,7 @@ configure :build do
   # out the following two lines has been known to help
   activate :minify_css
   activate :minify_javascript
+  activate :minify_html
   # activate :relative_assets
   # activate :asset_hash
   # activate :gzip
