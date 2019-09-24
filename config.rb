45c3148a640473a38f0c189503c38c2b3d0b4a0b
Adam Bachman <adam.bachman@gmail.com>
2019-09-24 13:37:12 -0400

    add error message display helper

--- config.rb
+++ config.rb
@@ -55,4 +55,5 @@ set :port, 4567
 
 helpers do
   require './lib/toc_data.rb'
+  require './lib/messages.rb'
 end
---
+++ lib/messages.rb
@@ -0,0 +1,3 @@
+def error_message(message)
+  "<p class='error-message-sample'>#{message}</p>"
+end
